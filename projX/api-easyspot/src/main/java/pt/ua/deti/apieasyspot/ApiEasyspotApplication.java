eb194446f16f6c6dd6c5bfb6ae9e69418272f634
Martim Gil <a.martimgil@gmail.com>
2026-03-20 15:43:57 +0000

    feat: initialize Spring Boot project with Maven setup and basic configurations

---
+++ projX/api-easyspot/src/main/java/pt/ua/deti/apieasyspot/ApiEasyspotApplication.java
@@ -0,0 +1,13 @@
+package pt.ua.deti.apieasyspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEasyspotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiEasyspotApplication.class, args);
+    }
+
+}
