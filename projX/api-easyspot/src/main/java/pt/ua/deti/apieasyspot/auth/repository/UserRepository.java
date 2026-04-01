f2b3b9317e4e5b4079234d976b6e430cdd50f764
Martim Gil <a.martimgil@gmail.com>
2026-04-01 23:56:53 +0100

    feat: add UserRepository for managing User entities and custom query method

---
+++ projX/api-easyspot/src/main/java/pt/ua/deti/apieasyspot/auth/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package pt.ua.deti.apieasyspot.auth.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pt.ua.deti.apieasyspot.auth.model.User;
+import pt.ua.deti.apieasyspot.vehicle.model.Vehicle;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByAuthentikUserId(String authentikUserId);
+}
+
