1951612dbac661cade74c852ada1eecafea26456
Martim Gil <a.martimgil@gmail.com>
2026-04-17 09:50:56 +0100

    feat: add UserFavoriteRepository for managing user favorite parking lots

---
+++ projX/api-easyspot/src/main/java/pt/ua/deti/apieasyspot/booking/repository/UserFavoriteRepository.java
@@ -0,0 +1,14 @@
+package pt.ua.deti.apieasyspot.booking.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pt.ua.deti.apieasyspot.booking.model.UserFavorite;
+import pt.ua.deti.apieasyspot.occupancy.model.ParkingLot;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserFavoriteRepository extends JpaRepository<UserFavorite, UUID> {
+
+    Optional<UserFavorite> findByUserAndParkingLot(UUID userId, UUID parkingLotId);
+    boolean existsByUserAndParkingLot(UUID userId, UUID parkingLotId);
+}
