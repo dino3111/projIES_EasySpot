d6887d3930db0b78b09f50256eed9b722114a764
Martim Gil <a.martimgil@gmail.com>
2026-04-22 00:14:37 +0100

    fix: remove unused imports in ApiEasyspotApplicationTests

--- projX/api-easyspot/src/test/java/pt/ua/deti/apieasyspot/ApiEasyspotApplicationTests.java
+++ projX/api-easyspot/src/test/java/pt/ua/deti/apieasyspot/ApiEasyspotApplicationTests.java
@@ -6,6 +6,9 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import pt.ua.deti.apieasyspot.billing.service.StripeService;
 
 @SpringBootTest(properties = {
     "STRIPE_API_KEY=sk_test_dummy",
