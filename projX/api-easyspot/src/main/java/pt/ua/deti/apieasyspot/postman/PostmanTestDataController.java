6af4586e35bec772592ee552a68a1c61b69d8b2a
Martim Gil <a.martimgil@gmail.com>
2026-04-22 10:52:06 +0100

    feat: update getSeedData method to include secondaryVehicleId and userId

--- projX/api-easyspot/src/main/java/pt/ua/deti/apieasyspot/postman/PostmanTestDataController.java
+++ projX/api-easyspot/src/main/java/pt/ua/deti/apieasyspot/postman/PostmanTestDataController.java
@@ -22,13 +22,9 @@ class PostmanTestDataController {
     Map<String, String> getSeedData() {
         return Map.of(
             "vehicleId", initializer.getVehicleId().toString(),
-<<<<<<< feat(api)/parking-space-reservation
+            "secondaryVehicleId", initializer.getSecondaryVehicleId().toString(),
             "parkId", initializer.getParkId().toString(),
             "userId", initializer.getUserId().toString()
-=======
-            "secondaryVehicleId", initializer.getSecondaryVehicleId().toString(),
-            "parkId", initializer.getParkId().toString()
->>>>>>> feat(api)/rest-api-endpoints
         );
     }
 }
