f9091ff5427d08e981e84459b2a3ab4700a2a349
Martim Gil <a.martimgil@gmail.com>
2026-04-22 09:21:42 +0100

    fix: update test properties and remove unused import in ApiEasyspotApplicationTests

--- projX/api-easyspot/src/test/java/pt/ua/deti/apieasyspot/ApiEasyspotApplicationTests.java
+++ projX/api-easyspot/src/test/java/pt/ua/deti/apieasyspot/ApiEasyspotApplicationTests.java
@@ -2,7 +2,6 @@ package pt.ua.deti.apieasyspot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import pt.ua.deti.apieasyspot.billing.service.StripeService;
@@ -10,9 +9,13 @@ import pt.ua.deti.apieasyspot.billing.service.StripeService;
 @SpringBootTest(properties = {
     "STRIPE_API_KEY=sk_test_dummy",
     "STRIPE_WEBHOOK_SECRET=whsec_dummy",
-    "spring.mail.host=localhost"
+    "spring.mail.host=localhost",
+    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
 })
-@Import(TestcontainersConfiguration.class)
 class ApiEasyspotApplicationTests {
 
     @MockitoBean
