f19248b63a055d5e4599f5719311299aef5b4ed1
Maria Moreira Mané <mariamoreiramane@ua.pt>
2026-05-12 23:41:52 +0100

    fixing bug

--- projX/api-easyspot/src/main/java/pt/ua/deti/apieasyspot/analytics/model/TechnicianParkAssignment.java
+++ projX/api-easyspot/src/main/java/pt/ua/deti/apieasyspot/analytics/model/TechnicianParkAssignment.java
@@ -3,8 +3,10 @@ package pt.ua.deti.apieasyspot.analytics.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import pt.ua.deti.apieasyspot.occupancy.model.ParkingLot;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
@@ -25,6 +27,10 @@ public class TechnicianParkAssignment {
 
     @Column(name = "parking_lot_id", nullable = false)
     private UUID parkingLotId;
+
+    @CreationTimestamp
+    @Column(name = "assigned_at", nullable = false, updatable = false)
+    private LocalDateTime assignedAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parking_lot_id", insertable = false, updatable = false)
